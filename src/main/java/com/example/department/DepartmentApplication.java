5e8baea895937ef0e1df92a242d3b59301a9d4ff
Fasi3898 <mohammed.fasiuddin@rtzen.ai>
2023-02-13 17:31:42 +0530

    updated gitignore

--- src/main/java/com/example/department/DepartmentApplication.java
+++ src/main/java/com/example/department/DepartmentApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DepartmentApplication {
 
+
+	// sample change, testing github
 	public static void main(String[] args) {
 		SpringApplication.run(DepartmentApplication.class, args);
 	}
